2f32ad12d4cf6a7c419bb0eedf1a51925f5e59aa
dsayles <dsayles@walleyesoftware.com>
2015-12-03 15:40:11 -0600

    minor changes to order

--- src/sample/phases/Phase.java
+++ src/sample/phases/Phase.java
@@ -21,20 +21,31 @@ public enum Phase {
     //
     COMBAT,
     //   ships fire from highest PS to lowest
+//    Declare Target
     //   choose target
     //   check firing arc
     //   check range
+
+//    Roll Attack Dice
     //   gather attack dice
     //   add attack die for range 1 if primary weapon
     //   roll attack dice  (weapon value)
-    //   modify post attack
+    //   modify post attack (Defender)
+    //   modify post attack (Attacker)
+
+//    Roll Defense Dice
     //   gather defense dice
     //   add defense die for range 3 if primary weapon
     //   add defense die for obstructions
     //   roll defense dice (agility)
-    //   modify post defense
+    //   modify post defense (Attacker)
+    //   modify post defense (Defender)
+
+//    Compare Results
     //   compare dice results
-    //   suffer damage to shield
+
+//    Deal Damage
+    //   suffer damage/crits to shields
     //   suffer damage to hull
     //   suffer crits to hull
     END
