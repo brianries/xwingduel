f9174294cfa9800dbc9c588e512cdfe0226374ff
dsayles <dsayles@walleyesoftware.com>
2016-08-20 22:11:49 -0500

    adding updated phase flow chart, this will need to be re-rigged into the code.

---
+++ src/main/java/phases/AttackPhase.java
@@ -0,0 +1,77 @@
+package phases;
+
+/**
+ * Created by dsayles on 8/20/16.
+ */
+public class AttackPhase {
+    // Text taken from updated attack phase flow chart
+
+//    1. “Declare Target” step
+//        i. Measure range to enemy ships and check attacker's firing arc
+//        ii. Attacker chooses weapon
+//        iii. Declare target of the attack
+//        iv. Pay cost to perform the attack (if applicable)
+//        v. Target of the attack becomes the defender
+//
+//    2. “Roll Attack Dice” step
+//        i. Identify number of attack dice (from Ship card or secondary weapon)
+//        ii. Resolve abilities that increase or decrease the number of attack dice (including Range Combat Bonus)
+//
+//    3. “Modify Attack Dice” step
+//        note: These abilities include adding, changing, and rerolling dice results.
+//            Dice can be modified multiple times, but each die can be rerolled only once.
+//        i. Defender resolves abilities that modify attack dice
+//        ii. Attacker resolves abilities that modify attack dice
+//
+//    4. “Roll Defense Dice” step
+//        i. Identify number of defense dice (from Ship card)
+//        ii. Resolve abilities that increase or decrease the number of defense dice (including Range Combat Bonus)
+//
+//    5. “Modify Defense Dice” step
+//        note: These abilities include adding, changing, and rerolling dice results.
+//            Dice can be modified multiple times, but each die can be rerolled only once.
+//        i. Attacker resolves abilities that modify defense dice
+//        ii. Defender resolves abilities that modify defense dice
+//
+//    6. “Compare Results” step
+//        i. Resolve abilities that occur “at the start of the ‘Compare Results’ step”
+//        ii. Cancel pairs of 󲁧 and 󲁄 results
+//        iii. Cancel pairs of 󲁨 and 󲁄 results
+//        iv. Determine whether the defender is hit by the attack or the attack misses
+//
+//
+//    Did the attack hit?
+//    yes: go to 7
+//
+//    7. “Deal Damage” step
+//        i. Remove shield tokens to cancel 󲁧 results
+//        ii. Remove shield tokens to cancel 󲁨 results
+//        iii. The defender is dealt facedown damage cards for remaining 󲁧 results
+//        iv. The defender is dealt faceup damage cards for remaining 󲁨 results
+//
+//
+//    no and after (7)
+//    Is this the first attack from a secondary weapon that says “Perform this attack twice”?
+//
+//    yes:  go to (2)
+//    no:  go to (8)
+//
+//    8. Abilities trigger that occur “after attacking” or “after defending” (that do not perform an attack)
+//        i. Resolve abilities of player with initiative
+//        ii. Resolve abilities of other player
+//
+//    9. Abilities trigger that occur “after attacking” or “after defending” that perform an attack
+//            (such as BTL-A4 Y-wing, Dengar [ship], Gunner, etc.)
+//        i. Resolve 1 ability of player with initiative
+//        ii. If you did not resolve an ability in step (i) resolve 1 ability of other player
+//
+//
+//    are you resolving an ability in step 9:
+//        yes: go to (1)
+//        no: go to (10)
+//
+//    10. Remove destroyed ships
+//        Remove the attacker if that ship was kept in play due to Simultaneous Attack Rule
+//        Simultaneous Attack Rule may prevent a destroyed ship from being removed
+
+}
